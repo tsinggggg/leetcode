c52877687201d8b5ba7ada5eabd1a172fd1f0785
tsinggggg <c29qian@uwaterloo.ca>
2016-08-11 20:56:00 -0500

    Guess Number Higher or Lower
    Guess Number Higher or Lower

--- Guess Number Higher or Lower.py
+++ Guess Number Higher or Lower.py
@@ -1,18 +1,23 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num):
+
 class Solution(object):
-    def isPowerOfThree(self, n):
-        if ((n==1)|(n==3)):
-            return True
-        else:
-            if (n<1):
-                return False
+    def guessNumber(self, n):
+        r=int((1+n)/2)
+        maxv=n+1
+        minv=0
+
+        while (guess(r)!=0):
+            if(guess(r)==1):
+                minv=r
+                r=min(max(int((r+maxv)/2),r+1),maxv-1)
             else:
-                temp=n%3
-                if(temp==0):
-                    return self.isPowerOfThree(n/3)
-                else:
-                    return False
+                maxv=r
+                r=max(min(int((r+minv)/2),r-1),minv+1)
+        return(r)
         """
         :type n: int
-        :rtype: bool
+        :rtype: int
         """
-        
