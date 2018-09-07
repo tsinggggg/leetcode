b13256fe7ad7b83ba6bb8ca0351e5e6f2a273323
tsinggggg <c29qian@uwaterloo.ca>
2018-09-07 10:02:52 -0500

    2nd commit

--- git_command.sh
+++ git_command.sh
@@ -2,3 +2,15 @@
 git remote -v
 
 #Git doesn’t store data as a series of changesets or deltas, but instead as a series of snapshots.
+
+#explanation of branch https://git-scm.com/book/en/v1/Git-Branching-What-a-Branch-Is
+
+#create a new branch, but not switch to this new branch 
+git branch new_branch
+
+#HEAD file pointing to the branch you’re on.
+
+#switch to a new branch 
+git checkout new_branch
+
+git push origin master
