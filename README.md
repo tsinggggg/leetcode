cb76eb300d32528c0ddf051406991bcfaf14a9e8
qc <c29qian@uwaterloo.ca>
2020-07-04 14:16:27 -0400

    reinstall

--- README.md
+++
@@ -1,1 +0,0 @@
-problem1:math.log(243,3)=4.9999999...instead of 5
--- permutation.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        import itertools
-        return [list(x) for x in itertools.permutations(nums)]
